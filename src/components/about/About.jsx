392b88caaa7c6a6a60865c68ec05afa6208dadde
UtkarshSonkar <utkarshsonkar8@gmail.com>
2023-04-23 23:31:05 +0530

    added profile image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import myprofile2 from "../../assets/myprofile2.jpg";
+import myprofile3 from "../../assets/myprofile3.jpeg";
 import Info from "./Info";
 //import CV from "../../assets/utkarshsonkar_resume-1.pdf";
 
@@ -10,7 +10,7 @@ const About = () => {
       <h2 className="section__title">About Me</h2>
       <span className="section__subtitle">My introduction</span>
       <div className="about__container container grid">
-        <img src={myprofile2} alt="" className="about__img" />
+        <img src={myprofile3} alt="" className="about__img" />
         <div className="about__data">
           <Info />
           <p className="about__description">
--- src/components/skills/WebDev.jsx
+++ src/components/skills/WebDev.jsx
@@ -91,6 +91,13 @@ const WebDev = () => {
           <div className="skills__data">
             <i class="bx bx-badge-check"></i>
             <div>
+              <h3 className="skills__name"></h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+            <div>
               <h3 className="skills__name">DSA</h3>
               <span className="skills__level">Intermediate</span>
             </div>
